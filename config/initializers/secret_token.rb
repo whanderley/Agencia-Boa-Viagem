e4fb297019ce7cd4c4924e43384a4a8b04339b2f
whanderley <whanderley@whanderley.(none)>
2012-02-14 16:29:07 -0200

    Projeto criado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgenciaBoaViagem::Application.config.secret_token = '7125640934a07b09c73929bb38b0eb56743d1fa7790ecdae98f3337021ca3407a1f37202d46e2608d147b097a86c23a7b2490d445945a3c8150aaeaf989e9630'
