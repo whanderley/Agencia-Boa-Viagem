aeb89e1eedc0203f57ed843191018a6f5ef9a89b
whanderley <whanderley@whanderley.(none)>
2012-02-14 18:38:21 -0200

    criado a edicao para clientes

--- spec/acceptance/gerenciar_cliente_spec.rb
+++ spec/acceptance/gerenciar_cliente_spec.rb
@@ -24,4 +24,24 @@ feature 'gerenciar cliente' do
     page.should have_content 'Endereco: Rua Dos bobos'
     page.should have_content 'Email: Zezinho@gmail.com'
   end
+
+  scenario 'alterar cliente' do #, :javascript => true do
+    cliente = Cliente.create nome: 'zezinho'
+    visit edit_cliente_path(cliente)
+   fill_in 'cliente_nome', :with => 'Zezinho'
+    fill_in 'cliente_cpf', :with => '123'
+    fill_in 'cliente_pontos', :with => '0'
+    fill_in 'cliente_endereco', :with => 'Rua Dos bobos'
+    fill_in 'cliente_email', :with => 'Zezinho@gmail.com'
+    fill_in 'cliente_telefone', :with => '99999999'
+        
+    click_button 'Salvar'
+   
+    page.should have_content 'Nome: Zezinho'
+    page.should have_content 'CPF: 123'
+    page.should have_content 'Pontos: 0'
+    page.should have_content 'Endereco: Rua Dos bobos'
+    page.should have_content 'Email: Zezinho@gmail.com'
+    
+  end
 end
