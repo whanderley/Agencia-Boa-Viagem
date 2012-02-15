c633d6f8a612734e63dbe5530cd357c1d355fb38
whanderley <whanderley@whanderley.(none)>
2012-02-15 14:45:03 -0200

    criado a edicao de servicos

--- spec/acceptance/gerenciar_servico_spec.rb
+++ spec/acceptance/gerenciar_servico_spec.rb
@@ -17,4 +17,16 @@ feature 'gerenciar servico' do
     page.should have_content 'Titulo: Massagem'
     page.should have_content 'Descrição: blabla'
   end
+
+  scenario 'alterar servico' do #, :javascript => true do
+    servico = Servico.create titulo: 'lol'  
+    visit edit_servico_path(servico)
+    fill_in 'servico_titulo', :with => 'Massagem'
+    fill_in 'servico_descricao', :with => 'blabla'
+
+    click_button 'Update Servico'
+
+    page.should have_content 'Titulo: Massagem'
+    page.should have_content 'Descrição: blabla'    
+  end
 end
